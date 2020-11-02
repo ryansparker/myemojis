de797e42e71d2e2eccb936800d4b246c74e77697
Ryan Parker <ryansparker@gmail.com>
2020-11-02 10:18:31 -0500

    buttons to add and remove emojis

--- index.js
+++ index.js
@@ -1,15 +1,81 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.getElementById("emojiContainer")
+const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
 
 console.log(emojiContainer)
 
-for (let i=0; i<myEmojis.length; i++) {
-    
-    const emoji = document.createElement("span")
-    emoji.textContent = myEmojis[i]
-    console.log(emoji)
-    emojiContainer.append(emoji)
-}
 
-emojiContainer.classList.add("magenta")
+function renderEmojis() {
+    
+    for (let i=0; i<myEmojis.length; i++) {
+        const emoji = document.createElement("span")
+        emoji.textContent = myEmojis[i]
+        console.log(emoji)
+        emojiContainer.append(emoji)
+    }
+
+}
+renderEmojis()
+
+// emojiContainer.classList.add("magenta")
+
+
+pushBtn.addEventListener('click', function() {
+   
+    //input value
+    let emojiInput = document.getElementById("emoji-input")
+
+   if (emojiInput.value) {
+       myEmojis.push(emojiInput.value)
+       emojiInput.value =""
+       emojiContainer.innerHTML = ""
+       console.log(myEmojis)
+       renderEmojis()
+   }
+})
+
+unshiftBtn.addEventListener('click', function() {
+
+    //input value  
+    let emojiInput = document.getElementById("emoji-input")
+
+   if (emojiInput.value) {
+       myEmojis.unshift(emojiInput.value)
+       emojiInput.value =""
+       emojiContainer.innerHTML = ""
+       console.log(myEmojis)
+       renderEmojis()
+   }
+})
+
+shiftBtn.addEventListener('click', function() {
+
+    //input value  
+    let emojiInput = document.getElementById("emoji-input")
+
+       myEmojis.shift(emojiInput.value)
+       emojiInput.value =""
+       emojiContainer.innerHTML = ""
+       console.log(myEmojis)
+       renderEmojis()
+
+})
+
+
+popBtn.addEventListener('click', function() {
+
+    //input value  
+    let emojiInput = document.getElementById("emoji-input")
+
+       myEmojis.pop(emojiInput.value)
+       emojiInput.value =""
+       emojiContainer.innerHTML = ""
+       console.log(myEmojis)
+       renderEmojis()
+
+})
+
 
